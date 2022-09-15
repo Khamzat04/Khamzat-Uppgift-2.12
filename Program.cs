61762b885b5005d3b6706499529bf4b35e3b45d6
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-15 13:10:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+// Upgift 2.12
+public class Program
+
+{
+	public static void Main()
+	{
+		int sAvgift = 300;
+		Console.WriteLine("hur många dagar tänker du hyra bilen");
+		int Dagar = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("hur många km ska du köra");
+		int Km = int.Parse(Console.ReadLine());
+
+		Console.WriteLine(sAvgift + Dagar * 500 - 500 + Km);
+	}
+}
